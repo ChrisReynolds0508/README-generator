38661e52fcefad151cb3bb2b59a2c588913b9883
Chrisreynolds0508 <Chris.reynolds0508@gmail.com>
2024-08-04 14:48:33 +1000

    updated javascript files

--- develop/index.js
+++ develop/index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
@@ -63,7 +63,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((data) => {
-        writeToFile('README.md', generateMarkdown(data));
+        writeToFile('README.md', generateMarkdown({...data}));
     });
 }
 
